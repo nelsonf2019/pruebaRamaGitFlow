71ef35630475af2b609433b2358fb371f26ba43c
Ezequiel <Ezequiel417@hotmail.com>
2024-06-12 21:55:18 -0300

    soy el home de index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
 export const Home=()=>{
     return(
         <div>
-            <h1>Sopy el home</h1>
+            <h1>Soy el home</h1>
             <form action="">
                 <text type="text" ></text>
                 <textarea name="" id=""></textarea>
