1b647cbc012d2bc319a701184c369f20feca6632
StevenGerman <germansteven2021@gmail.com>
2024-06-12 21:52:07 -0300

    componente prueba

---
+++ src/components/compoenetePrueba.tsx
@@ -0,0 +1,13 @@
+// src/components/TestComponent.js
+import React from 'react';
+
+const TestComponent = () => {
+  return (
+    <div>
+      <h1>Componente de Prueba</h1>
+      <p>Este es un componente de prueba.</p>
+    </div>
+  );
+};
+
+export default TestComponent;
